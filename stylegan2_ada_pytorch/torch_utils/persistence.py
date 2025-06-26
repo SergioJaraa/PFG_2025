cc1965f39ee0aaf26c94441a5224f9666dff1844
SergioJaraa <sjaramillomonasterio@gmail.com>
2025-06-26 15:21:29 +0200

    CHANGES

--- stylegan2_ada_pytorch/torch_utils/persistence.py
+++ stylegan2_ada_pytorch/torch_utils/persistence.py
@@ -21,6 +21,8 @@ import copy
 import uuid
 import types
 import stylegan2_ada_pytorch.dnnlib as dnnlib
+from stylegan2_ada_pytorch.training.networks import FullyConnectedLayer
+
 
 
 #----------------------------------------------------------------------------
